8b249305c999e828f6f47812ce5a8fe4585ece83
박상희 <hee@hee.local>
2025-04-22 13:16:09 +0900

    TEMPLATE 추가

--- deals/views.py
+++ deals/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .crawler import run_crawler
+from .models import Game
 
 def index_view(request):
     return render(request, "index.html")
 
 def run_crawler_view(request):
     run_crawler()
-    return redirect('/api/game/')
+    return redirect('game-list')
+
+def game_list(request):
+    games = Game.objects.all()
+    return render(request, 'deals/game_list.html', {'games': games})
+
+def game_detail(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+    reviews = game.reviews.all()
+    return render(request, 'deals/game_detail.html', {'game': game, 'reviews':reviews})
--- steamsale/urls.py
+++ steamsale/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from deals.views import index_view  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('deals.urls')),
     path('api/', include('deals_api.urls')),
-    path('deals/', include('deals_api.urls')),
-    path('', include('deals_api.urls')),
 ]
