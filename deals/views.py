3dd72c8ec51c6339abc6cc254ca1150b90a3f7cc
Dongak Park <dongakp@gmail.com>
2025-04-22 12:52:18 +0900

    run_crawler_view 함수 post method 추가

--- deals/views.py
+++ deals/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import redirect, render
 from .crawler import run_crawler
+from django.http import HttpResponse
 
 def index_view(request):
     return render(request, "index.html")
 
 def run_crawler_view(request):
-    run_crawler()
-    return redirect('/api/game/')
+    if request.method == 'POST':
+        category = request.POST.get('category')
+        count = int(request.POST.get('count'))
+        run_crawler(category, count)
+        return redirect('/api/game/')
+    return HttpResponse("비정상적인 접근입니다.")
