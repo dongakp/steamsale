f7176c9238003aa1a50e82a2ab758ea08ddb4377
minshik0705 <112872144+minshik0705@users.noreply.github.com>
2025-04-23 11:08:11 +0900

    adding dashboard template
    http://127.0.0.1:8000/dashboard/
    여기에 접속을 해야할 것 같습니다
    그래프 수치 변화는 chart-area-demo.js 랑 chart-bar-demo.js에서 하면 되는데 막대그래프 데모는 x라벨 제한이 결려있어서 글자가 안 나올 수 도 있어요 그거 푸는 코드 사용하시면 될거 같습니다

--- steamsale/urls.py
+++ steamsale/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from deals.views import index_view  
+from deals.views import index_view, index_alt_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('deals.urls')),
     path('api/', include('deals_api.urls')),
+    path('dashboard/', index_alt_view, name='dashboard'),
 ]
